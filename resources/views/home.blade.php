3648cee3678f8e721f0a3008e6ae47d782864254
Dale <xthedalex@gmail.com>
2016-10-30 18:41:53 -0500

    Home Template and file permissions

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,65 @@
 
 @section('content')
     <div class="container">
-        <h1>Hello, world!</h1>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="panel panel-grey">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Card 1</h3>
+                </div>
+                <div class="panel-body">
+                    Placeholder
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="panel panel-grey">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Card 2</h3>
+                </div>
+                <div class="panel-body">
+                    Placeholder
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="panel panel-grey">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Card 3</h3>
+                </div>
+                <div class="panel-body">
+                    Placeholder
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="panel panel-grey">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Card 4</h3>
+                </div>
+                <div class="panel-body">
+                    Placeholder
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="panel panel-grey">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Card 5</h3>
+                </div>
+                <div class="panel-body">
+                    Placeholder
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="panel panel-grey">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Card 6</h3>
+                </div>
+                <div class="panel-body">
+                    Placeholder
+                </div>
+            </div>
+        </div>
     </div>
-
 @endsection
